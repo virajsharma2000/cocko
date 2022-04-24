f4ca256645858438e903b0e873c87702291973c4
virajsharma2000 <kaushik.kalpana@gmail.com>
2022-04-24 18:11:15 +0530

    my more improovement in twit programming language

--- twit.py
+++ twit.py
@@ -25,9 +25,23 @@ if x == 'replace(string)':
  print(string.replace(string,y))
 if x == 'remove(string)':
  print(string.replace(string,''))
-
-
-
+if x == 'check_big_string_and_small_string()':
+ length = int(len(string))
+ if length > 5:
+  print('big string')
+ else:
+  print('small string')
+if x == 'sort(string)':
+ array2 = []
+ string2 = ''
+ for s in string:
+  array2.append(s)
+ array2.sort()
+ for so in array2:
+  string2 += so
+ print(string2)
+ print
+  
 
 
 
